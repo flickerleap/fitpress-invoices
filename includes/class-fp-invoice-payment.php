de96a9d24b0ec5e223fc238044ed092ce8d427f4
Seagyn <seagyn@digitalleap.co.za>
2017-04-10 13:35:00 +0200

    Remove force payment

--- includes/class-fp-invoice-payment.php
+++ includes/class-fp-invoice-payment.php
@@ -41,13 +41,13 @@ class FP_Payment {
 
 		$membership = FP_Membership::get_user_membership( $member_id );
 
-		if ( is_user_logged_in() && 'active' != $membership_status && 'Once Off' != $membership['term'] && ! isset( $wp->query_vars['checkout'] ) ) :
+		/*if ( is_user_logged_in() && 'active' != $membership_status && 'Once Off' != $membership['term'] && ! isset( $wp->query_vars['checkout'] ) ) :
 			fp_add_flash_message( __( 'Please, make payment to activate your membership again.', 'fitpress' ), 'error' );
 			wp_redirect( fp_checkout_url() );
 			exit;
 		elseif ( is_user_logged_in() && 'active' == $membership_status && ! apply_filters( 'fitpress_payment_token', true ) ) :
 			fp_add_flash_message( __( 'Please, <a href="' . fp_checkout_url() . '">add your payment method</a>. You will not be billed now.', 'fitpress' ), 'error' );
-		endif;
+		endif;*/
 	}
 
 	protected function get_defaults() {
